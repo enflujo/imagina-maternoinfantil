9513efd01e919e5690a3cb66b115b2e87b9a521e
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-04-26 17:01:53 -0500

    🔨 Aplanar datos, reducir peso

--- servidor.js
+++ servidor.js
@@ -1,74 +1,75 @@
 const { writeFileSync } = require('fs');
 const xlsx = require('xlsx');
-const rutaDenominador = './datos/porcentaje_anticoncepcion_denominador.xlsx';
-const rutaNumerador = './datos/porcentaje_anticoncepcion_numerador.xlsx';
 
+/**
+ * Guarda datos de JS a un archivo .json
+ *
+ * @param {object} json Objeto o Array que se quiere guardar en archivo JSON.
+ * @param {string} nombre Nombre del archivo.
+ */
+const guardarJSON = (json, nombre) => {
+  writeFileSync(`./src/datos/${nombre}.json`, JSON.stringify(json, null, 2));
+};
+
+const extraerNombreCodigo = texto => {
+  const arr = texto.split('-');
+  const codigo = arr[0].trim();
+  const nombre = arr[1].trim();
+  return { nombre, codigo };
+};
+
+const rutaDenominador = './src/datos/porcentaje_anticoncepcion_denominador.xlsx';
+const rutaNumerador = './src/datos/porcentaje_anticoncepcion_numerador.xlsx';
 const excelDenominador = xlsx.readFile(rutaDenominador);
 const excelNumerador = xlsx.readFile(rutaNumerador, { header: 1, range: 2 });
 
 const denominador = xlsx.utils.sheet_to_json(excelDenominador.Sheets.Hoja1);
 const numerador = xlsx.utils.sheet_to_json(excelNumerador.Sheets.Hoja1);
-denominador.pop()
+denominador.pop();
 numerador.pop();
 
-const datos = {};
+const datos = [];
 
-function extraerNombreCodigo(texto) {
-  const arr = texto.split('-');
-  const codigo = arr[0].trim();
-  const nombre = arr[1].trim();
-  return { nombre, codigo }
-}
-
-const guardarJSON = (json, nombre) => {
-  writeFileSync(`./pruebas/${nombre}.json`, JSON.stringify(json, null, 2));
-};
-
-function estructurarDatos(variable, llave) {
-  variable.forEach(obj => {
+function estructurarDatos(datosFuente, llave) {
+  datosFuente.forEach(obj => {
     const { Anno, Departamento, Municipio, Total } = obj;
 
-    if (!datos.hasOwnProperty(Anno)) {
-      datos[Anno] = [];
-    }
-
-    const departamento = extraerNombreCodigo(Departamento);
-    let departamentoI = datos[Anno].findIndex(d => d.departamento === departamento.nombre);
-
-    if (departamentoI < 0) {
-      datos[Anno].push({
-        departamento: departamento.nombre,
-        codigo: departamento.codigo,
-        municipios: []
-      });
-
-      departamentoI = datos[Anno].length - 1;
-    }
-
     const municipio = extraerNombreCodigo(Municipio);
-    let municipioI = datos[Anno][departamentoI].municipios.findIndex(d => d.municipio === municipio.nombre);
+    const departamento = extraerNombreCodigo(Departamento);
+    let municipioI = datos.findIndex(mun => mun.codMun === municipio.codigo);
 
     if (municipioI < 0) {
-      datos[Anno][departamentoI].municipios.push({
+      datos.push({
         municipio: municipio.nombre,
-        codigo: municipio.codigo,
+        codMun: municipio.codigo,
+        departamento: departamento.nombre,
+        codDep: departamento.codigo,
+        datos: {}
       });
-      municipioI = datos[Anno][departamentoI].municipios.length - 1;
+
+      municipioI = datos.length - 1;
     }
 
-    datos[Anno][departamentoI].municipios[municipioI][llave] = Total;
+    if (!datos[municipioI].datos[Anno]) {
+      datos[municipioI].datos[Anno] = {
+        numerador: 0,
+        denominador: 0,
+        porcentaje: 0
+      };
+    }
 
-    const numerador = datos[Anno][departamentoI].municipios[municipioI].numerador;
-    const denominador = datos[Anno][departamentoI].municipios[municipioI].denominador;
+    datos[municipioI].datos[Anno][llave] = Total;
+
+    const numerador = datos[municipioI].datos[Anno].numerador;
+    const denominador = datos[municipioI].datos[Anno].denominador;
 
     if (numerador && denominador) {
-      datos[Anno][departamentoI].municipios[municipioI].porcentaje = ((numerador / denominador) * 100);
+      datos[municipioI].datos[Anno].porcentaje = (numerador / denominador) * 100;
     }
-
   });
 }
 
 estructurarDatos(denominador, 'denominador');
-estructurarDatos(numerador, 'numerador')
+estructurarDatos(numerador, 'numerador');
 
-guardarJSON(datos, 'anticoncepcion')
+guardarJSON(datos, 'anticoncepcion');
