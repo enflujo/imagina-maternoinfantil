1c73a15eeda513357a2d13601f9faafd2bd6afb9
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-07-01 08:00:20 -0500

    🔨 Crear módulo menú indicadores

--- src/views/VistaTablero.vue
+++ src/views/VistaTablero.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref } from 'vue';
-import Mapa from '@/components/MapaElemento.vue';
-import fuentes from '@/utilidades/fuentes';
+import Mapa from '../components/MapaElemento.vue';
+import MenuIndicadores from '../components/MenuIndicadores.vue';
+import fuentes from '../utilidades/fuentes';
 const datos = ref([]);
 const formaDepartamentos = ref([]);
 const rutaBase = 'https://enflujo.com/bodega';
@@ -13,6 +14,7 @@ async function cambiarIndicador(indiceIndicador) {
     const { nombreArchivo } = fuentes[indiceIndicador];
     const respuesta = await fetch(`${rutaBase}/mi_v2/${nombreArchivo}-departamentos.json`);
     datos.value = await respuesta.json();
+
     indicadorActual.value = indiceIndicador;
   }
 }
@@ -28,49 +30,10 @@ cambiarIndicador(0);
 
 <template>
   <main>
-    <ul id="menuIndicadores" class="contenedorMenu">
-      <li
-        v-for="(indicador, i) in fuentes"
-        @click="cambiarIndicador(i)"
-        :key="indicador.nombreIndicador"
-        class="botonIndicador"
-        :class="i === indicadorActual ? 'activo' : ''"
-      >
-        {{ indicador.nombreIndicador }}
-      </li>
-    </ul>
+    <MenuIndicadores :cambiarIndicador="cambiarIndicador" :indicadorActual="indicadorActual" />
 
-    <Mapa :geojson="formaDepartamentos" :datos="datos" :año="año"></Mapa>
+    <Mapa :geojson="formaDepartamentos" :datos="datos" :año="año" />
   </main>
 </template>
 
-<style lang="scss" scoped>
-@import '@/assets/constantes.scss';
-
-#menuIndicadores {
-  width: 35vw;
-  font-size: 0.9em;
-  font-family: $fuenteTexto;
-  position: fixed;
-}
-
-.contenedorMenu {
-  list-style: none;
-}
-
-.botonIndicador {
-  cursor: pointer;
-  padding: 0.5em;
-  border-bottom: 1px solid;
-  opacity: 0.8;
-
-  &:hover {
-    opacity: 0.6;
-  }
-
-  &.activo {
-    opacity: 1;
-    font-weight: bold;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
