9270f3ce680e89b964c3f3a680f3648150ddeac9
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-07-05 01:19:53 -0500

    ➕ opción de vista por nivel dep o mun

--- src/views/VistaTablero.vue
+++ src/views/VistaTablero.vue
@@ -1,20 +1,20 @@
 <script setup>
 import { ref } from 'vue';
-import Mapa from '../components/MapaElemento.vue';
-import MenuIndicadores from '../components/MenuIndicadores.vue';
+import Mapa from '../componentes/MapaElemento.vue';
+import MenuIndicadores from '../componentes/MenuIndicadores.vue';
 import fuentes from '../utilidades/fuentes';
 const datos = ref([]);
 const formaDepartamentos = ref([]);
 const rutaBase = 'https://enflujo.com/bodega';
 const año = ref(2015);
 const indicadorActual = ref(-1);
+const nivel = 'departamentos';
 
 async function cambiarIndicador(indiceIndicador) {
   if (indicadorActual.value !== indiceIndicador) {
     const { nombreArchivo } = fuentes[indiceIndicador];
-    const respuesta = await fetch(`${rutaBase}/mi_v2/${nombreArchivo}-departamentos.json`);
+    const respuesta = await fetch(`${rutaBase}/mi_v2/${nombreArchivo}-${nivel}.json`);
     datos.value = await respuesta.json();
-
     indicadorActual.value = indiceIndicador;
   }
 }
@@ -24,14 +24,13 @@ async function cargarGeojson(nombre) {
   formaDepartamentos.value = await respuesta.json();
 }
 
-cargarGeojson('departamentos');
+cargarGeojson(nivel);
 cambiarIndicador(0);
 </script>
 
 <template>
   <main>
     <MenuIndicadores :cambiarIndicador="cambiarIndicador" :indicadorActual="indicadorActual" />
-
     <Mapa :geojson="formaDepartamentos" :datos="datos" :año="año" />
   </main>
 </template>
