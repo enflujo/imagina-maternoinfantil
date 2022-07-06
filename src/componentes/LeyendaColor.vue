16c8086ef54854d83c6b8efa2cb9cf9e355348e3
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-07-06 18:53:08 -0500

    🏗️ Inicio módulos complementarios

---
+++ src/componentes/LeyendaColor.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { escalaColores } from '../utilidades/ayudas';
+
+const props = defineProps({
+  colores: Object,
+});
+
+const rango = ref([]);
+
+const mapearColor = escalaColores(0, 100, props.colores[0], props.colores[1]);
+
+onMounted(() => {
+  for (let i = 0; i <= 100; i++) {
+    rango.value.push(mapearColor(i));
+  }
+});
+</script>
+
+<template>
+  <div id="leyendaRango">
+    <span v-for="(color, i) in rango" :key="`color-${i}`" :style="`background-color:${color}`"></span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+span {
+  display: inline-block;
+  height: 10px;
+  width: 2px;
+}
+</style>
