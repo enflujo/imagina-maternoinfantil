0581d4893b8e2672d3bdff75be8882050a3e5e7a
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-07-22 19:18:07 -0500

    🚧 Publicar versión en desarrollo

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  publicDir: 'publico',
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
+  },
 });
